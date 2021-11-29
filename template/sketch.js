e7c2bc5c8a325d6a94ed52f295ccf943b5cc1bc4
declanbbbbb <declan.bnbg@gmail.com>
2021-11-29 11:39:02 -0600

    about to change triangle stuff, backing up

--- template/sketch.js
+++ template/sketch.js
@@ -45,11 +45,4 @@ function keyPressed() {
   p2 = random(height);
   p3 = random(width);
   p4 = random(height);
-  // charBackground(0);
-  // for (let i = 0; i < 1; i++) {
-  //   charStroke(0.3);
-  //   let args = [random(width), random(height), random(width), random(height), random(width), random(height)];
-  //   // charLineTriangle(args[0], args[1], args[2], args[3], args[4], args[5]);
-  //   charTriangle(mouseX, mouseY, args[2], args[3], args[4], args[5]);
-  // }
 }
